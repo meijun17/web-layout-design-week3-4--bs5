fab8907d0819330fd93b0c9d85e168e00c36c498
Mei Jun Chen <wdxlf8992@gmail.com>
2022-08-21 21:02:55 +0800

    Update 2022-08-21T13:02:49.857Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,30 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  $(".nav__link").each(function (index) {
+    $(this).click(function () {
+      //點選觸發事件
+      $(".nav__link").removeClass("active");
+      $(".nav__link").eq(index).addClass("active");
+    });
+  });
+});
+
+function isEditStyle() {
+  $("#ckeditor").toggleClass("d-none");
+  $(".others__comment").toggleClass("d-none");
+  $(".page-btn").toggleClass("d-none");
+  $(".ckeditor-btn").toggleClass("d-none");
+}
+
+$("#reply-btn").on("click", function () {
+  isEditStyle();
+});
+$("#cancel-btn").on("click", function () {
+  isEditStyle();
+});
+var editor = document.querySelector('#editor');
+editor && ClassicEditor.create(document.querySelector('#editor'))["catch"](function (error) {
+  console.error(error);
 });
 //# sourceMappingURL=all.js.map
