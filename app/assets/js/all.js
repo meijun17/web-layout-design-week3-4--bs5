d0ddca2bccf0543e609ad4bebe54125dba48b57d
Mei Jun Chen <wdxlf8992@gmail.com>
2022-08-21 21:03:11 +0800

    add

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,34 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+  $(".nav__link").each(function(index){
+    $(this).click(function(){ //點選觸發事件
+      $(".nav__link").removeClass("active");
+      $(".nav__link").eq(index).addClass("active");
+    });
+  })
 });
+
+function isEditStyle(){
+  $("#ckeditor").toggleClass("d-none");
+  $(".others__comment").toggleClass("d-none");
+  $(".page-btn").toggleClass("d-none");
+  $(".ckeditor-btn").toggleClass("d-none");
+}
+
+$("#reply-btn").on("click",()=>{
+  isEditStyle();
+})
+
+$("#cancel-btn").on("click",()=>{
+  isEditStyle();
+})
+
+
+
+
+
+const editor = document.querySelector('#editor');
+editor && ClassicEditor
+.create( document.querySelector( '#editor' ) )
+.catch( error => {
+    console.error( error );
+} );
